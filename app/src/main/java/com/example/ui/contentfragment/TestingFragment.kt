db8ba8f36048b2ea5f3d8e7ec1db89f6e51ff28b
Anuj <anujrai04082002.0@gmail.com>
2021-05-23 18:04:00 +0530

    Added Testing Layout Status:Successfully

--- app/src/main/java/com/example/ui/contentfragment/TestingFragment.kt
+++ app/src/main/java/com/example/ui/contentfragment/TestingFragment.kt
@@ -1,7 +1,15 @@
 package com.example.ui.contentfragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.covidapp.R
+import com.example.covidapp.databinding.TestingFragmentBinding
 
-class TestingFragment:Fragment(R.layout.testing_fragment) {
+class TestingFragment : Fragment(R.layout.testing_fragment) {
+    private lateinit var binding: TestingFragmentBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = TestingFragmentBinding.bind(view)
+    }
 }
